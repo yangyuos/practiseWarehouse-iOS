712422deaa6f509b84c82e4dd4834a49f9755f53
杨羽 <296900684@qq.com>
2016-08-18 13:32:11 +0800

    新建项目

---
+++ ARCAR/ARCAR/ARCAR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARCAR
+//
+//  Created by 杨羽 on 16/8/18.
+//  Copyright © 2016年 杨羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
